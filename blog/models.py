8a2b1b8f0d0465c2be258cd8163c3c033668b464
masuda <teruko441@gmail.com>
2019-12-09 15:41:12 +0900

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Post(models.Model):
     auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
